99cea3f6171f68467e156f41458f8be2cb90d5e0
Jakub Plachý <placjaku@gmail.com>
2025-02-04 19:21:37 +0100

    Initial Commit

---
+++ ChatGPTChallengeFebruary/ChatGPTChallengeFebruaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTChallengeFebruaryApp.swift
+//  ChatGPTChallengeFebruary
+//
+//  Created by Jakub Plachy on 04.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTChallengeFebruaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPTChallengeFebruaryTests/ChatGPTChallengeFebruaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTChallengeFebruaryTests.swift
+//  ChatGPTChallengeFebruaryTests
+//
+//  Created by Jakub Plachy on 04.02.2025.
+//
+
+import Testing
+@testable import ChatGPTChallengeFebruary
+
+struct ChatGPTChallengeFebruaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
